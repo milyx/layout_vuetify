271b5cca03b2a4cf3ac341719812a20aa17860ce
milagros acosta <milyarias91@gmail.com>
2022-04-07 15:01:16 -0400

    app changes

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div>
       <v-app-bar color="deep-purple accent-4" dense dark>
         <v-app-bar-nav-icon></v-app-bar-nav-icon>
-        <v-toolbar-title>Page title</v-toolbar-title>
+        <v-toolbar-title>My App</v-toolbar-title>
         <v-spacer></v-spacer>
         <v-btn>
           <v-text>Inicio</v-text>
